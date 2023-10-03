7baff16112b01abbff596e5650b1c6763efebc72
Inquest007 <raju.bakathatla@gmail.com>
2023-10-03 18:32:03 +0530

    added-welcome

--- restapi-aws/src/main/java/com/example/demo/aws/Awscntrlr.java
+++ restapi-aws/src/main/java/com/example/demo/aws/Awscntrlr.java
@@ -9,5 +9,10 @@ public class Awscntrlr {
 	@GetMapping("/aws")
 	public String awsFirst(){
 		return "Welcome to AWS!!"; }
+	
+	@GetMapping
+	public String hello() {
+		return "Hello Welcome!!";
+	}
 
 }
