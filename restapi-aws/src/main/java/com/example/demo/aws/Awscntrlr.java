3c10c41719959d1a6bc06d0cf7482761d4ed6d72
sanju <sanju@Sanju1729>
2023-10-03 17:59:34 +0530

    first commit

---
+++ restapi-aws/src/main/java/com/example/demo/aws/Awscntrlr.java
@@ -0,0 +1,13 @@
+package com.example.demo.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Awscntrlr {
+	
+	@GetMapping("/aws")
+	public String awsFirst(){
+		return "Welcome to AWS!!"; }
+
+}
---
+++ restapi-aws/src/main/java/com/example/demo/aws/RestapiAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiAwsApplication.class, args);
+	}
+
+}
